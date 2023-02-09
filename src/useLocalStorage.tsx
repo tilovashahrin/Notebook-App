882e72b6b66bc5f6fdc74cbc470ade79b9cde560
Tilova <tilova97@gmail.com>
2023-02-09 17:49:08 -0500

    documentation

--- src/useLocalStorage.tsx
+++ src/useLocalStorage.tsx
@@ -5,15 +5,18 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
         const jsonValue = localStorage.getItem(key)
         if (jsonValue == null) {
             if (typeof initialValue === "function") {
-               return (initialValue as () => T)()
+                return (initialValue as () => T)()
             }
             return initialValue
         }
         return JSON.parse(jsonValue)
     })
 
+    //save our data in localStorage every time our value or key changes
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [value, key])
+
+    //returning an array thats always type T and type of the setValue
     return [value, setValue] as [T, typeof setValue]
 }
