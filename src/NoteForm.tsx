97d607072d2d189286c7caef7ea8c1cc1cc0c962
Tilova <tilova97@gmail.com>
2023-02-17 13:15:05 -0500

    navigate

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ReactSelectCreatable from 'react-select/creatable'
 import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid"
@@ -15,14 +15,16 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate()
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault()
         onSubmit({
             title: titleRef.current!.value,
             markdown: markdownRef.current!.value,
-            tags: []
+            tags: selectedTags,
         })
+        navigate("..")
     }
 
     return (
